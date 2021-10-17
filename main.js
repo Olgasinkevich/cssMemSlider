6e43b8f82ccbed2620bb83334111100066aeea06
Olgasinkevich <sinkevichweb@gmail.com>
2021-10-17 20:10:35 +0300

    feat: add text slider 17 окт 2021 г. 20:10:35

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 const buttons = document.querySelectorAll('.button');
-const imageContainer = document.querySelector('.images');
 
+const imageContainer = document.querySelector('.images');
 const images = document.querySelectorAll('.images img');
 let imageWidth = parseInt(window.getComputedStyle(images[0]).width);
+
+const textContainer = document.querySelector('.texts');
+const texts = document.querySelectorAll('.text');
+let textWidth = parseInt(window.getComputedStyle(texts[0]).width);
+
 
 window.addEventListener('resize', () => {
   imageWidth = parseInt(window.getComputedStyle(images[0]).width);
@@ -18,25 +23,7 @@ buttons.forEach((item, index) => {
 })
 
 function moveSlide(indexNext) {
-/*  let defaultX = -parseInt(window.getComputedStyle(imageContainer).left);
-
-  if (defaultX < indexNext * imageWidth)  {
-    let id = null;
-    id = setInterval(() => {
-      if (defaultX < indexNext * imageWidth) {
-        defaultX += 100;
-        imageContainer.style.left = `${imageContainer.style.left - 100}px`;
-        console.log('defaultX', defaultX)
-        console.log('imageContainer.style.left', imageContainer.style.left)
-      } else {
-        clearInterval(id);
-      }
-    }, 200);
-  } else {
-
-  }*/
-
-
   imageContainer.style.left = `${-indexNext * imageWidth}px`;
+  textContainer.style.left = `${-indexNext * textWidth}px`;
 }
 
